cd7480b8092c3e963e23a7590c11c8fd4f618adc
Eoin4321 <eoinhamill1919@gmail.com>
2024-02-11 18:12:51 +0000

    Finished Question 5.

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,6 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Eoin Hamill
+ *  Class Group: GD2a
  */
 
 public class CA3_Question5
@@ -8,6 +12,64 @@ public class CA3_Question5
 
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+        Queue<String> takeoff = new LinkedList<String>();
+        Queue<String> landing = new LinkedList<String>();
+        boolean on = true;
+        while(on)
+        {
+            System.out.println("\ntype: takeoff flightSymbol - To add a flight to take off\n" +
+                    "type: land flightSymbol -  To add a flight to land" +
+                    "\ntype: next - to run the software" +
+                    "\ntype: quit - to end the software");
+            String taken = input.next();
 
+            if(taken.equals("quit"))
+            {
+                on=false;
+                break;
+            }
+            //If users inputs next
+            else if(taken.equals("next"))
+            {
+                //Checking to see if the priority queue is not empty. If it is empty planes can take off.
+                if(!landing.isEmpty())
+                {
+                    System.out.println("Now landing plane "+landing.poll());
+                }
+                //
+                else if(!takeoff.isEmpty())
+                {
+                    System.out.println("Now taking off plane "+takeoff.poll());
+                }
+                //Telling the user the next flight
+                if(!landing.isEmpty())
+                {
+                    System.out.println("The next prioritized flight is landing of plane "+landing.peek());
+                }
+                else if(!takeoff.isEmpty())
+                {
+                    System.out.println("The next prioritized flight is take off of plane "+takeoff.peek());
+                }
+                else if(takeoff.isEmpty()&& landing.isEmpty())
+                {
+                    System.out.println("No flights due to take off");
+                }
+            }
+            else if(taken.equals("land"))
+            {
+                landing.add(input.next());
+                System.out.println("Added new landing plane to queue");
+            }
+            else if(taken.equals("takeoff"))
+            {
+                takeoff.add(input.next());
+                System.out.println("Added new takeoff plane to queue");
+            }
+            else
+            {
+                System.out.println("Invalid Input.");
+            }
+        }
     }
 }
